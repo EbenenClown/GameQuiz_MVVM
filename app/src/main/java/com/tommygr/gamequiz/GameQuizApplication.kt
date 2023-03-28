f0eeb373ccac953169b9fa779ebaab9165212b71
Tommy <Tommy.Gruessmer@googlemail.com>
2023-03-28 15:24:20 +0200

    added Hilt and Firebase dependencies

--- app/src/main/java/com/tommygr/gamequiz/GameQuizApplication.kt
+++ app/src/main/java/com/tommygr/gamequiz/GameQuizApplication.kt
@@ -1,7 +1,9 @@
 package com.tommygr.gamequiz
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class GameQuizApplication: Application() {
 
     override fun onCreate() {
