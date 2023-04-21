bee12bfb41f8b688682b0ec9e61ed95c25676a38
Tommy <Tommy.Gruessmer@googlemail.com>
2023-04-21 09:23:55 +0200

    added api interface

---
+++ app/src/main/java/com/tommygr/gamequiz/data/source/remote/FirebaseAPI.kt
@@ -0,0 +1,54 @@
+package com.tommygr.gamequiz.data.source.remote
+
+import com.tommygr.gamequiz.data.QuizElement
+import com.tommygr.gamequiz.data.Statistic
+import com.tommygr.gamequiz.data.User
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+
+interface FirebaseAPI {
+    @GET("/quizelements")
+    fun getAll(): List<QuizElement>
+
+    @GET("/quizelements/{id}")
+    fun getElementById(@Path("id")id: String): QuizElement
+
+    @POST("/quizelements/")
+    fun saveElement(element: QuizElement)
+
+    @PUT("quizelements/{id}")
+    fun updateElement(@Path("id")id: String, @Body element: QuizElement)
+
+    @GET("/statistics/{id}")
+    fun getStatisticById(@Path("id")userId: String): Statistic
+
+    @POST("/statistics/")
+    fun saveStatistic(statistic: Statistic)
+
+    @PUT("statistics/{id}")
+    fun updateStatistic(@Path("id")id: String, @Body statistic: Statistic)
+
+    @DELETE("statistics/{id}")
+    fun deleteStatistic(@Path("id")id: String, @Body statistic: Statistic)
+
+    @GET("/users/{id}")
+    fun getUserById(@Path("id")id: User): User
+
+    @POST("/users/")
+    fun saveNewUser(user: User)
+
+    @PUT("users/{id}")
+    fun updateUser(@Path("id")id: String, @Body user: User)
+
+    @DELETE("users/{id}")
+    fun deleteUser(@Path("id")id: String, @Body user: User)
+
+
+}
